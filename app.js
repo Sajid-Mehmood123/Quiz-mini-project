c35298d4a58a7273a75ae9fee0393cfc51b153cc
Sajid-Mehmood123 <mssajidali45@gmail.com>
2024-07-29 15:11:31 +0500

    final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ["B", "B", "B", "B"];
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -17,5 +18,17 @@ form.addEventListener("submit", (e) => {
     }
   });
 
-  console.log(score);
+  //  show result to page
+  scrollTo(0, 0);
+  result.classList.remove("d-none");
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
